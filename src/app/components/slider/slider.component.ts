43b01aec18b53df7a08fb2e241f4d0c277e1e3b2
sherif <sherif.elshamaaa@gmail.com>
2022-04-19 02:22:40 +0200

    sec commit

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ])
+  ]
 })
 export class SliderComponent implements OnInit {
+  @Input() items: Movie[] = [];
 
-  constructor() { }
+  currentSlideIndex: number = 0;
 
-  ngOnInit(): void {
+  today = new Date(Date.now());
+  compareDates(date1: any, date2: any) {
+    if (date1 === null || date2 === null) {
+      return false;
+    }
+    const d1 = new Date(date1)
+    const d2 = date2
+    return d1 > d2;
   }
 
+  ngOnInit(): void {
+    setInterval(()=>{
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    },5000)
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -9,6 +9,6 @@ export class MoviesService {
   constructor(private http: HttpClient) { }
 
   getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=46f2bb8cb504071f0bffb7e1b1ab8e42')
+    return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=46f2bb8cb504071f0bffb7e1b1ab8e42')
   }
 }
